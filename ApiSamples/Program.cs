d63bd40e112b19eea3d120d4a289679d9f8c1b78
Pavel Pochobut <pavel.pochobut@gmail.com>
2015-11-26 16:06:50 +0300

    added TFS integration samples

--- ApiSamples/Program.cs
+++ ApiSamples/Program.cs
@@ -9,9 +9,18 @@ namespace ApiSamples
     {
         static void Main(string[] args)
         {
-            var client = new HttpClient(new Uri("http://{youraccount}.go.easyprojects.net/rest/v1/"), 
+            var client = new HttpClient(new Uri("http://{youraccount}.go.easyprojects.net/rest/v1/"),
                 "{username}", "{password}");
 
+            // basic samples
+            CreateProjectAndTask(client);
+
+            // TFS Integration
+            TFSSamples.RunSamples(client);
+        }
+
+        private static void CreateProjectAndTask(HttpClient client)
+        {
             var user = client.Query<User>()
                     .Where(u => u.Login == "{username}") // Replace with real login
                     .ToList().Single();
@@ -21,16 +30,16 @@ namespace ApiSamples
 
             // add project memeber
             var member = client.Post<ProjectMember>(
-                string.Format("projects/{0}/members", p.ProjectID), 
-                new ProjectMember 
+                string.Format("projects/{0}/members", p.ProjectID),
+                new ProjectMember
                 {
-                    ProjectID = p.ProjectID, 
-                    UserID = user.UserID 
+                    ProjectID = p.ProjectID,
+                    UserID = user.UserID
                 });
 
             // get all ProjectMembers for Project
             var members = client.QueryAsResource<ProjectMember>(
-                string.Format("projects/{0}/members", p.ProjectID), 
+                string.Format("projects/{0}/members", p.ProjectID),
                 p.ProjectID.ToString()
                 ).ToList();
 
@@ -39,11 +48,11 @@ namespace ApiSamples
             // Create Task
             // Supported TaskTypes are: 1 - Task, 2 - Issue, 3 - Request
             var task_1 = client.Post<Task>(
-                new Task 
+                new Task
                 {
-                    Name = "Task 1", 
-                    ProjectID = p.ProjectID, 
-                    TaskTypeID = 1 
+                    Name = "Task 1",
+                    ProjectID = p.ProjectID,
+                    TaskTypeID = 1
                 });
 
             // assign Task 1 to a project member
@@ -54,21 +63,21 @@ namespace ApiSamples
 
             //create subtask for Task 1
             var subtask = client.Post<Task>(
-                new Task 
+                new Task
                 {
-                    Name = "Task 11", 
-                    ProjectID = p.ProjectID, 
-                    ParentID = task_1.TaskID, 
-                    TaskTypeID = 1 
+                    Name = "Task 11",
+                    ProjectID = p.ProjectID,
+                    ParentID = task_1.TaskID,
+                    TaskTypeID = 1
                 });
 
             // specify TaskID for add TaskMessage, ProjectId for add ProjectMessage
             var message = client.Post<Message>(
-                new Message 
-                { 
-                    TaskID = task_1.TaskID, 
-                    UserID = 1, 
-                    PostDate = DateTime.Now, 
+                new Message
+                {
+                    TaskID = task_1.TaskID,
+                    UserID = 1,
+                    PostDate = DateTime.Now,
                     MessageText = "Message Text"
                 });
         }
---
+++ ApiSamples/TFSSamples.cs
@@ -0,0 +1,71 @@
+﻿using EasyProjects.Client;
+using EasyProjects.ClientModel.Entities;
+using Microsoft.TeamFoundation.Client;
+using Microsoft.TeamFoundation.WorkItemTracking.Client;
+using System;
+using System.Linq;
+using Project = Microsoft.TeamFoundation.WorkItemTracking.Client.Project;
+
+namespace ApiSamples
+{
+
+    class TFSSamples
+    {
+        class TaskData
+        {
+            public string Name;
+        }
+
+        internal static void RunSamples(HttpClient client)
+        {
+            Uri collectionUri = new Uri("http://tfshost:8080/tfs/DefaultCollection/");
+            TfsTeamProjectCollection tpc = new TfsTeamProjectCollection(collectionUri);
+            WorkItemStore workItemStore = tpc.GetService<WorkItemStore>();
+
+            TaskData sampleDataFromTFS = GetSomeWorkItemFromTFS(workItemStore);
+            CreateTaskInEP(client, sampleDataFromTFS);
+
+            TaskData sampleDataFromEP = GetSomeTaskFromEP(client);
+            CreateWorkItemInTFS(workItemStore, sampleDataFromEP);
+        }
+
+        private static void CreateWorkItemInTFS(WorkItemStore workItemStore, TaskData taskData)
+        {
+            Project teamProject = workItemStore.Projects["TestProject"];
+            WorkItemType taskItemType = teamProject.WorkItemTypes["Task"];
+
+            WorkItem newTask = new WorkItem(taskItemType)
+            {
+                Title = taskData.Name
+            };
+
+            newTask.Save();
+        }
+
+        private static TaskData GetSomeTaskFromEP(HttpClient client)
+        {
+            var tasks = client.Query<Task>().Take(1).ToArray();
+
+            return new TaskData { Name = tasks[0].Name };
+        }
+
+        private static TaskData GetSomeWorkItemFromTFS(WorkItemStore workItemStore)
+        {
+            WorkItemCollection queryResults = workItemStore.Query("SELECT [Title] FROM WorkItems");
+
+            return new TaskData { Name = queryResults[0].Title };
+        }
+
+        private static void CreateTaskInEP(HttpClient client, TaskData sampleDataFromTFS)
+        {
+            // Supported TaskTypes are: 1 - Task, 2 - Issue, 3 - Request
+            var task_1 = client.Post<Task>(
+                new Task
+                {
+                    Name = sampleDataFromTFS.Name,
+                    ProjectID = 1, // replace with real project ID
+                    TaskTypeID = 1
+                });
+        }
+    }
+}
